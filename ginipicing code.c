f89e44a4897cab247d17e2b0bcc61a593d47a49a
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2019-03-26 00:02:36 +0600

    ginipicing.c
    tread mark of IIT

---
+++ ginipicing code.c
@@ -0,0 +1,22 @@
+#include<stdio.h >
+int main()
+
+{
+
+
+
+
+
+    printf("######\t######\t######\n");
+    printf("  ##  \t  ##  \t  ##\n");
+    printf("  ##  \t  ##  \t  ##\n");
+    printf("  ##  \t  ##  \t  ##\n");
+    printf("  ##  \t  ##  \t  ##\n");
+    printf("  ##  \t  ##  \t  ##\n");
+    printf("  ##  \t  ##  \t  ##\n");
+    printf("######\t######\t  ##\n");
+
+    return 0;
+
+
+}
